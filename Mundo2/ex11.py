31728b3167afc2db38641cfb03e19271fb27cbec
Bruno Souza <souzabrunu@gmail.com>
2025-12-09 11:19:10 -0300

    exercicos

---
+++ Mundo2/ex11.py
@@ -0,0 +1,7 @@
+
+saldo = 2000
+saque = 3000
+
+
+status = 'Sucesso' if saldo >= saque else 'Falha!'
+print(f'{status} ao realizar o saque!')
---
+++ Mundo2/ex9.py
@@ -0,0 +1,11 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print('valor sacado!')
+        print('Retire seu dinheiro na boca do caixa.')
+
+    print('Obrigado por ser nosso cliente')
+
+
+sacar(500)
