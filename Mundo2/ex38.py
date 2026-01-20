df5c0d940b2d7ad201cf77ccde9f8cc6cd9adffa
Bruno Souza <souzabrunu@gmail.com>
2026-01-20 11:51:35 -0300

    exercicios

--- Mundo2/ex38.py
+++ Mundo2/ex38.py
@@ -1,29 +1,35 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+
 def lista_de_produtos():
 
     lista = ['Pente', 'Bolsa', 'Bola']
 
     while True:
-        novo_produto = input('Digite um novo produto: ').capitalize()
+        novo_produto = input(Fore.CYAN + 'Digite um novo produto: ').strip().capitalize()
 
         if not novo_produto.isalpha():
-            print('Digite novamente, sem espaços e numeros!')
+            print(Fore.RED + '❌ Digite novamente, sem espaços e numeros!')
         elif novo_produto in lista:
 
-            print('O produto já está na lista!')
+            print(Fore.YELLOW + '⚠️ O produto já está na lista!')
         else:
             lista.append(novo_produto)
             break
 
     while True:
-        remover = input('Digite um produto para remover: ')
-        if not remover:
-            print('O produto precisa esta na lista.')
+        remover = input(Fore.CYAN + 'Digite um produto para remover: ').strip().capitalize()
+        if remover in lista:
+            lista.remove(remover)
+            print()
+            print(Fore.GREEN + '✅ Produto removido com sucesso!')
             break
         else:
-            remover in lista
-            lista.remove(remover)
-
-            print('Produto removido com sucesso!')
+            print(Fore.RED + '❌ O produto não está na lista. Tente novamente!')
+    print()
+    print(Fore.CYAN + f'📦 Lista final de produtos: {lista}\n')
 
 
 lista_de_produtos()
